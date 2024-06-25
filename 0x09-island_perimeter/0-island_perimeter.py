9d9a17981ce5ce5bd61e65244769a7c0e3bb0d80
fasasimayokun <maykay.fash@gmail.com>
2024-06-25 15:41:22 +0000

    my commit for island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a py script Island Perimeter Problem
+"""
+
+
+def island_perimeter(grid):
+    """
+    this func calculates the perimeter of the island described in grid
+    Args:
+        grid: 2d list of integers containing 0(water) or 1(land)
+    Return:
+        the perimeter of the island
+    """
+
+    prm = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    prm += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    prm += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    prm += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    prm += 1
+    return prm
